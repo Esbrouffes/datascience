8aef5e67563dc7ac4c74d04febae0db3dfd3f988
Esbrouffes <thibault_dew@hotmail.fr>
2019-06-02 22:05:41 +0200

    working ? or NOT BITCH

--- challenge3/challenge3_evolution.py
+++ challenge3/challenge3_evolution.py
@@ -31,17 +31,18 @@ elevation_cells = np.array(list(csv.reader(open("./DataPreparation/elevation_cel
 
 
 def toGraph(M):
+	""" a function to convert a matrix or array into a graph, taking into account the elevation and population"""
 	Hj={}
 	G=nx.Graph()
 	lines,columns=M.shape
 	for i in range(lines):
-		for j in range(columns):
-			k=i*columns+j
+		for j in range(columns): 
+			k=i*columns+j #creating the nodes of the graph with nodes numbered by creascent number of column 
 			G.add_node(k)
-			Hj[k]=round(popu_cells[i][j])
+			Hj[k]=round(popu_cells[i][j]) # inserting the population cells 
 
 	for i in range(lines):
-		for j in range(columns):
+		for j in range(columns): # we will now add the adges between the nodes, considering their position in the original matrix, weight being the angle between two cells.
 			k=i*columns+j
 			if i==0:#first line
 				if j==0: #left top corner
@@ -93,20 +94,14 @@ def toGraph(M):
 
 
 	return G,Hj
-t=time.time()
-G,Hj=toGraph(elevation_cells)
-a=0
-for i in G.nodes():
-	if i%5==0:
-		a+=1
-#print(a) ( tests basiques de durée etc )
 
-#print(time.time()-t)
-#print(G.edges())
+
+G,Hj=toGraph(elevation_cells) #we create the graph 
 
 
 
-"""GRAPH TEST : 
+
+"""GRAPH TEST : This graph was created so we could test how the zombie epidemics spreads with time.
 G=nx.Graph()
 Hj={}
 G.add_nodes_from(range(36))
@@ -141,7 +136,7 @@ for i in G.nodes():
 
 """ ZOMBIES HAVE A LIFE EXEPCTANCY """
 def vieillissement(zombies,i):
-	"""effectue le vieillissement d'une liste de zombies"""
+	"""gets zombies older"""
 	if i in zombies:
 		zombies[i].insert(0,0)
 		deads=zombies[i].pop()
@@ -151,38 +146,37 @@ def vieillissement(zombies,i):
 
 """zombies will stay if there are no humans around"""	
 def spreading(cell):
-	"""how will we use fill the graph ? """
-	""" good idea nb of zombies in an array, filled with 0s """
+	"""this functions make the spreading of zombies from the cell given in arguments to its neighbors"""
 
 	if cell in Zj:
 		H_cell=0
 		lambda_list=[]
 		for i in G.neighbors(cell):
 			if i in Hj: H_cell+=Hj[i]
-			lambda_list	.append(1-G[cell][i]['weight'])
+			lambda_list	.append(1-G[cell][i]['weight']) # we will use it to compute the geographical slope factor. It is the angle divided by the mean angle, this way 100% of the zombies move
 			#print((G[cell][i]['weight']))
 		lambda_mean=np.mean(lambda_list)
 		if lambda_mean==0:
-			lambda_mean=1 #so no division by zero 
+			lambda_mean=1 #so no division by zero -> we are looking at absolute values therefore if mean is 0, they are all 0 , it will be 0/1 = 0. Moreover, due to the symetry of the slope factor, it isn't possible to have a "hole" or " peak" with zombies inside ! 
 		
 	
 		
 		### STEP ONE 
-		N=sum(Zj[cell]) #nombre de zombies dans la cellule originale
+		N=sum(Zj[cell]) #number of zombies in the original cell
 		if H_cell!=0:
 			for i in G.neighbors(cell):
-				if i in Zj_1:
+				if i in Zj_1: #it means the cell is already in the zombie land
 					for j in range(len(Zj[cell])):
 						
 						Zj_1[i][j]+=round(Zj[cell][j]*Hj[i]/H_cell * (1-G[cell][i]['weight'])/lambda_mean)
 						Zj_1[cell][j]-=round(Zj[cell][j]*Hj[i]/H_cell * (1-G[cell][i]['weight'])/lambda_mean )
 						if Zj_1[cell][j] <0: Zj_1[cell][j] = 0 # so the round doesn't make negative numbers of zombies 
 				else: 
-					Zj_1[i]=[]
+					Zj_1[i]=[] #we have to create the cell
 					for j in range(len(Zj[cell])):
-						Zj_1[i].append(round(Zj[cell][j]*Hj[i]/H_cell * (1-G[cell][i]['weight'])/lambda_mean))
-						Zj_1[cell][j]-=round(Zj[cell][j]*Hj[i]/H_cell * (1-G[cell][i]['weight'])/lambda_mean )
-						if Zj_1[cell][j] <0: Zj_1[cell][j] = 0 
+						Zj_1[i].append(round(Zj[cell][j]*Hj[i]/H_cell * (1-G[cell][i]['weight'])/lambda_mean)) #round is because there are no such things as half zombies ! 
+						Zj_1[cell][j]-=round(Zj[cell][j]*Hj[i]/H_cell * (1-G[cell][i]['weight'])/lambda_mean ) #we have to take them back from the original cell (actually it's from its copy that will be kept at the end of the propagation algorithm)
+						if Zj_1[cell][j] <0: Zj_1[cell][j] = 0 # the round can cause nb of zombies <0 which isn't good ! 
 			
 		 
 		
@@ -192,7 +186,7 @@ def spreading(cell):
 
 
 
-"""for i in G.neighbors(cell): 
+"""for i in G.neighbors(cell):    A PART OF THE CODE THAT ISN'T USED ANYMORE BUT WAS WITH OUR ORIGINAL FROMAT
 			if i in Zj_1:
 				for j in range(len(Zj[cell])):
 					if Zj[cell][j][0]==Zj_1[i][j][0]:
@@ -229,7 +223,7 @@ def zombified(cell):
 		N=sum(Zj_1[cell])
 		M=Hj[cell]
 		if 10*N<=M:
-			Hj[cell]-=10*N
+			Hj[cell]-=10*N #we take the humans back
 			Zj_1[cell][0]=10*N
 			
 			return 10*N
@@ -254,7 +248,7 @@ def killed(cell):
 					if Zj_1[cell][j]!=0: lenzombies+=1
 				for j in range(len(Zj_1[cell])):
 					if Zj_1[cell][j]!=0: 
-						Zj_1[cell][j]-=round(10*M*Zj_1[cell][j]/N)
+						Zj_1[cell][j]-=round(10*M*Zj_1[cell][j]/N) #zombies have to die proportionnaly to their ages ! 
 						if Zj_1[cell][j] <0: Zj_1[cell][j] = 0  #zombies die no matter the age, zombies that don't exist don't die : only - for ages with a nb of zombies != 0 
 				return 10*M
 			elif M!=0:
@@ -267,11 +261,14 @@ def killed(cell):
 	else: return 0
 
 def nb_zombies(zombies):
+	"""computes the number of zombies in a zombie dictionnary ( Zj or Zj_1) """
 	Z=0
-	for i in zombies:
-		Z+=sum(zombies[i])
+	for i in G.nodes():
+		if i in zombies:
+			Z+=sum(zombies[i])
 	return Z
 def nb_humans(humans):
+	"""compute the number of humans """ 
 	H=0
 	for i in humans:
 		H+=humans[i]
@@ -280,7 +277,7 @@ def nb_humans(humans):
 
 
 def daily_fights(G,Zj,Zj_1,Hj):
-	Total_H=0
+	""" does the whole process that happens during one day ( the several steps ) """
 	Zold=nb_zombies(Zj_1)
 	for c in G.nodes():
 		vieillissement(Zj,c)
@@ -309,39 +306,64 @@ def daily_fights(G,Zj,Zj_1,Hj):
 
 	
 	
-	return Total_H,nb_zombies(Zj_1),Znew,Z_killed,Zold #il faudrait calculer toutes les valeurs en fin, plutot
+	return nb_zombies(Zj_1),Znew,Z_killed,Zold #these are important values if we want to make sure the number of zombies evolves correctly in a simple graph : no zombies are created or deleted without reason ! 
 
 def reducing(zombies):
+	"""we can pop all the cells that are empty of zombies but referenced in a dictionnary ( they are like : 'j': [0,0,0, ..., 0])"""
 	to_pop=[]
 	for j in zombies:
 		if zombies[j]==[0]*15:
-			to_pop.append(j) #otherwise it changes the size of dictionnary between iterations 
+			to_pop.append(j) #otherwise it changes the size of dictionnary between iterations ( not elegant but it works fine ! )
 
 	for j in to_pop:
 		zombies.pop(j)
 
-def naive(humans):
+
+def naive_h(humans):
+	"""returns the 20 most populated human cells at a time """
 	return sorted(humans,key=humans.__getitem__)[len(humans)-20:len(humans)]
 
+def ego_level_simple_centrality(G,r):
+	sums={}
+	for i in G.nodes():
+		total_local =0
+		for j in nx.ego_graph(G,i,r):
+			total_local+=Hj[j]
+		sums[i]=total_local
+	return sorted(sums,key=sums.__getitem__)[len(sums)-20:len(sums)]#return the best cells according to this model 
 
-	"""on peut avoir des compteurs pour certifier que le résultat est bon ( autant de zombies avant que après - nouveaux + ceux tués )
+
+	
+
+"""Tests 
+
+
+
+
+global Zj
+global Zj_1
+Zj={}
+Zj_1={}
+
+
+Zj[58400]=[30,0,0,0,0,0,0,0,0,0,0,0,0,0,0] # 30 zombies in the cells in the corner, and we have to fill the humans with 110 humans in each cell so the evolution is easily computable with hand
+#print(Zj)
+Zj_1[58400]=[30,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 """
-
-"""Tests unitaires """
-
-"""Test vieillissement : ok
+"""
+Test vieillissement : ok
 for i in range(13):
 	for i in G.nodes():
 		vieillissement(Zj,i)
 print(Zj)
--> fonctionnel
+-> fonctionnal
 """
 
 """Test spreading : ok 
 for i in G.nodes():
 	spreading(i)
 print(Zj_1)
--> fonctionnel """
+-> fonctionnal """
 
 
 """Test zombified: ok  
@@ -349,58 +371,27 @@ for i in G.nodes():
 	vieillissement(Zj_1,i)
 	zombified(i)
 
--> fonctionnel
+-> fonctionnal
 """
 
 """Test killed : ok """
 
 
-"""
-
-
-
-global Zj
-global Zj_1
-Zj={}
-Zj_1={}
-
-
-
-Zj[58400]=[30,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-#print(Zj)
-Zj_1[58400]=[30,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-
-for _ in range():
+""" daily fights test : ok 
+for _ in range(5):
 	print("-------------------day ------------------",_)   THESE IS TO SEE THE EVOLUTION 
 	print(daily_fights(G,Zj,Zj_1,Hj))
 	Zj=copy.deepcopy(Zj_1)
-	
-
-"""
-
-
-
-
-
-"""IS CENTRALITY ENOUGH ??? WE NEED TO CONSIDER THE NUMBER OF HUMANS AND THE CENTRALITY IN ORDER TO DO THAT
-
-What's left to do : 
-
-
--considering elevation ! 
--geographical slope
--finding all the shortest paths
--having models for the cells we want to protect ! 
-
-Problem ??? -> The length of the dictionnary is bigger and bigger -> it takes a lot of time to copy it -> reducing 
-
 
 
 """
 
-####  MAIN 
+####  MAIN -> the real maps and real numbers of humans 
 
 #Initialisation
+
+
+
 lines,columns=elevation_cells.shape
 rize=columns*141+284 
 brest= columns*38+33 
@@ -408,65 +399,85 @@ brest= columns*38+33
 Hj[rize]=0
 global Zj
 global Zj_1
-Zj={}
-Zj_1={}
 
-Zj[rize]=[95000,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+Hu_plots=[]
+Zu_plots=[]
+for r in range(3):
+	G,Hj=toGraph(elevation_cells) #we create the graph 
+	Zj={}
+	Zj_1={}
 
-Zj_1[rize]=[95000,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+	Zj[rize]=[95000,0,0,0,0,0,0,0,0,0,0,0,0,0,0] # " the whole population of rize is contamined"
 
-days=0
+	Zj_1[rize]=[95000,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-#beginning
-t=time.time()
-while brest not in Zj:
-	days+=1
-	daily_fights(G,Zj,Zj_1,Hj)
-	reducing(Zj_1)
-	Zj=copy.deepcopy(Zj_1)
+	days=0
+	timelapse=[days]
+	Hu=[nb_humans(Hj)]
+	Zu=[nb_zombies(Zj_1)]
+	#beginning
+	t=time.time()
+	#while days<=500: or 
+	while brest not in Zj: # we stop the spreading when they arrive there, but we could continue, by commenting this line and uncommenting the previous one ! 
+
+
+		print(nb_zombies(Zj_1))
+		print(nb_humans(Hj)) # for few statistics
+		days+=1
+		timelapse.append(days) # to see the evolution
+		daily_fights(G,Zj,Zj_1,Hj) # the daily routine
+		reducing(Zj_1) #
+		Zj=copy.deepcopy(Zj_1)
+		Hu.append(nb_humans(Hj))
+		Zu.append(nb_zombies(Zj_1))
 		
-	print("------------------------------------------------------ day ",days, " -------------------------------------------------------------------")
-	if days==61: 
-		print("two months after the beginning of the zombie apocalypse")
-		print(naive(Hj))
-		for i in naive(Hj):
-			#print(Hj[i])
-			if i in G:G.remove_node(i) #the zombies are killed ! 
-			 #pop ? we don't know what to do -> store them in another dictionnary
+		print("------------------------------------------------------ day ",days, " ----------------------------------------------------------------")
+		if days==61: 
+			for i in ego_level_simple_centrality(G,r):
+				print(Hj[i])
+				if i in G:G.remove_node(i)
+
+	print("BREST IS ZOMBIFIED !!! This happens at day ",days )
+	print(nb_zombies(Zj_1))
+	print(nb_humans(Hj))
+
+	print("time to execute = ", time.time()-t)
 
 
-	if days==71:
-		print(nb_zombies(Zj_1))
-		print(nb_humans(Hj))
-		
+	Hu_plots.append(Hu)
+	Zu_plots.append(Zu)
 
-	if days==91:
-		print(nb_zombies(Zj_1))
-		print(nb_humans(Hj))
-
-	if days==121:
-		print(nb_zombies(Zj_1))
-		print(nb_humans(Hj))
-	if days==161:
-		print(nb_zombies(Zj_1))
-		print(nb_humans(Hj))
-
+plt.figure()
+for human in Hu_plots:
+	plt.plot(timelapse,human)
+plt.figure()
+for zombies in Zu_plots:
+	plt.plot(timelapse,zombies)
+plt.show()
+			
 
 
 """
-evaluation : days before reaching brest, number of humans killed before troops vs after, number of zombies 10 days after before / after , number of zombies 30 days after. 
-Quantitatif vs qualitatif 
+	evaluation : days before reaching brest, number of humans killed before troops vs after, number of zombies 10 days after before / after , number of zombies 30 days after. 
+	Quantitatif vs qualitatif 
 
 
-"""
-
-
-
+	"""
 
 
 
-print("BREST IS ZOMBIFIED !!! This happens at day ",days )
-print(nb_zombies(Zj_1))
-print(nb_humans(Hj))
 
-print("time to execute = ", time.time()-t)
+
+""" 
+
+
+RESULT : 
+
+2 month after, 
+
+2123775.0
+2871845.0
+
+
+2123775.0
+2813804.0"""
